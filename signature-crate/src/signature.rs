13674773a515eb56e928c5e65bd1c6df874437ed
Tony Arcieri <bascule@gmail.com>
2019-10-11 09:13:50 -0700

    Revert "signature-crate: Replace DigestSignature with derive attr"
    This reverts commit 48e33d8758be5ce71c8c4787ed92a8a725d09f5f.
    
    After updating my downstream consumers to try to use this, I encountered
    an important case where this falls down: deriving `Signer` and
    `Verifier` on generic types where the `Digest` *can only* be generically
    specified as an associated type, as the `yubihsm` crate is doing here:
    
    https://github.com/tendermint/yubihsm-rs/blob/develop/src/ecdsa/signer.rs#L22
    
    The goal of switching to a derive attribute was to make this
    functionality more flexible and eliminate the need for a marker trate,
    but in this particular case (one I personally consider very important)
    it had the opposite effect.

--- signature-crate/src/signature.rs
+++ signature-crate/src/signature.rs
@@ -22,3 +22,19 @@ pub trait Signature: AsRef<[u8]> + Debug + Sized {
         self.as_slice().into()
     }
 }
+
+/// Marker trait for `Signature` types computable as `S(H(m))`
+///
+/// - `S`: signature algorithm
+/// - `H`: hash (a.k.a. digest) function
+/// - `m`: message
+///
+/// For signature types that implement this trait, a blanket impl of
+/// `Signer` will be provided for all types that `impl DigestSigner`
+/// along with a corresponding impl of `Verifier` for all types that
+/// `impl DigestVerifier`.
+#[cfg(feature = "digest")]
+pub trait DigestSignature: Signature {
+    /// Preferred `Digest` algorithm to use when computing this signature type.
+    type Digest: digest::Digest;
+}
