a3ca6bb73e2ac9280e7317564ed40adb9bafa6f0
Tony Arcieri <bascule@gmail.com>
2019-10-27 12:22:57 -0700

    ecdsa: Initial curves (NIST P-256/384, secp256k1)
    Adds a `Curve` trait representing short Weirstrass curves suitable for
    use with ECDSA, along with an initial set of curves and their associated
    private scalar size.

--- ecdsa/src/lib.rs
+++ ecdsa/src/lib.rs
@@ -1,1 +1,12 @@
-#![crate_name = "ecdsa"]
+//! Elliptic Curve Digital Signature Algorithm (ECDSA) as specified in
+//! [FIPS 186-4] (Digital Signature Standard)
+
+#![no_std]
+#![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
+    html_root_url = "https://docs.rs/ecdsa/0.0.0"
+)]
+
+pub mod curve;
