409cec8a072ad4a521ebbde19cf2ee76afdc7e08
Tony Arcieri <bascule@gmail.com>
2019-10-28 13:59:36 -0700

    ecdsa: Asn1Signature <-> FixedSignature translation
    Adapts the following code from BearSSL to support translation between
    ASN.1 DER encoded and fixed size (a.k.a. "raw") signatures.
    
    This additionally allows eagerly checking that ASN.1 DER-encoded
    signatures are well-formed.

--- ecdsa/src/curve.rs
+++ ecdsa/src/curve.rs
@@ -7,11 +7,12 @@ pub mod secp256k1;
 pub use self::{nistp256::NistP256, nistp384::NistP384, secp256k1::Secp256k1};
 
 use core::{fmt::Debug, ops::Add};
+use generic_array::typenum::Unsigned;
 
 /// Elliptic curve in short Weierstrass form suitable for use with ECDSA
 pub trait Curve: Debug + Default + Send + Sync {
     /// Size of an integer modulo p (i.e. the curve's order) when serialized
     /// as octets (i.e. bytes). This also describes the size of an ECDSA
     /// private key, as well as half the size of a fixed-width signature.
-    type ScalarSize: Add; // `Add<Self>` impl from typenum used for doubling
+    type ScalarSize: Unsigned + Add; // `Add<Self>` impl from typenum used for doubling
 }
