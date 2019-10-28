f62882326168553b5cdfd440d10bdb5fe0b16007
Tony Arcieri <bascule@gmail.com>
2019-10-28 11:49:28 -0700

    ecdsa: Reorganize size-related trait bounds
    Moves all trait bounds related to `FixedSignature` from the `Curve`
    trait to the `FixedSignature` type.
    
    This avoids compounding the complexity of the trait bounds on `Curve`
    when ASN.1 DER signatures are added.

--- ecdsa/src/fixed_signature.rs
+++ ecdsa/src/fixed_signature.rs
@@ -7,8 +7,7 @@ use signature::Error;
 
 /// Size of a fixed sized signature: double that of the scalar size
 // TODO(tarcieri): use typenum's `Double` op or switch to const generics
-pub type FixedSignatureSize<C> =
-    <<C as Curve>::ScalarSize as Add<<C as Curve>::ScalarSize>>::Output;
+pub type Size<ScalarSize> = <ScalarSize as Add<ScalarSize>>::Output;
 
 /// Fixed-sized (a.k.a. "raw") ECDSA signatures: serialized as fixed-sized
 /// big endian scalar values with no additional framing.
@@ -16,18 +15,20 @@ pub type FixedSignatureSize<C> =
 pub struct FixedSignature<C>
 where
     C: Curve,
-    FixedSignatureSize<C>: ArrayLength<u8>,
+    C::ScalarSize: Add<C::ScalarSize>,
+    Size<C::ScalarSize>: ArrayLength<u8>,
 {
-    bytes: GenericArray<u8, FixedSignatureSize<C>>,
+    bytes: GenericArray<u8, Size<C::ScalarSize>>,
 }
 
 impl<C> signature::Signature for FixedSignature<C>
 where
     C: Curve,
-    FixedSignatureSize<C>: ArrayLength<u8>,
+    C::ScalarSize: Add<C::ScalarSize>,
+    Size<C::ScalarSize>: ArrayLength<u8>,
 {
     fn from_bytes(bytes: impl AsRef<[u8]>) -> Result<Self, Error> {
-        if bytes.as_ref().len() == <FixedSignatureSize<C>>::to_usize() {
+        if bytes.as_ref().len() == <Size<C::ScalarSize>>::to_usize() {
             Ok(Self {
                 bytes: GenericArray::clone_from_slice(bytes.as_ref()),
             })
@@ -40,7 +41,8 @@ where
 impl<C> AsRef<[u8]> for FixedSignature<C>
 where
     C: Curve,
-    FixedSignatureSize<C>: ArrayLength<u8>,
+    C::ScalarSize: Add<C::ScalarSize>,
+    Size<C::ScalarSize>: ArrayLength<u8>,
 {
     fn as_ref(&self) -> &[u8] {
         self.bytes.as_slice()
@@ -50,7 +52,8 @@ where
 impl<C> fmt::Debug for FixedSignature<C>
 where
     C: Curve,
-    FixedSignatureSize<C>: ArrayLength<u8>,
+    C::ScalarSize: Add<C::ScalarSize>,
+    Size<C::ScalarSize>: ArrayLength<u8>,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
