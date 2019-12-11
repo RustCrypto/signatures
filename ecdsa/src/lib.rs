4f97bf0498c66cc5299d7754e6ffbef47aacbe0d
Tony Arcieri <bascule@gmail.com>
2019-12-11 09:44:19 -0800

    ecdsa v0.3.0

--- ecdsa/src/lib.rs
+++ ecdsa/src/lib.rs
@@ -2,10 +2,20 @@
 //! [FIPS 186-4][1] (Digital Signature Standard)
 //!
 //! This crate doesn't contain an implementation of ECDSA itself, but instead
-//! contains [`ecdsa::Asn1Signature`] and [`ecdsa::FixedSignature`] types
-//! generic over an [`ecdsa::Curve`] type which other crates can use in
-//! conjunction with the [`signature::Signer`] and [`signature::Verifier`]
-//! traits.
+//! contains [`Asn1Signature`] and [`FixedSignature`] types which are generic
+//! over elliptic [`Curve`] types (e.g. `NistP256`, `NistP384`, `Secp256k1`)
+//! which can be used in conjunction with the [`signature::Signer`] and
+//! [`signature::Verifier`] traits to provide signature types which are
+//! reusable across multiple signing and verification provider crates.
+//!
+//! Transcoding between [`Asn1Signature`] and [`FixedSignature`] of the same
+//! [`Curve`] type is supported in the form of simple `From` impls.
+//!
+//! Additionally, the [`PublicKey`] and [`SecretKey`] types, also generic
+//! over elliptic curve types, provide reusable key types, sourced from the
+//! [`elliptic-curve`][2] crate. The [`PublicKey`] type supports both
+//! compressed and uncompressed points, and for the P-256 curve in particular
+//! supports converting between the compressed and uncompressed forms.
 //!
 //! These traits allow crates which produce and consume ECDSA signatures
 //! to be written abstractly in such a way that different signer/verifier
@@ -19,13 +29,14 @@
 //! - Const generics(!)
 //!
 //! [1]: https://csrc.nist.gov/publications/detail/fips/186/4/final
+//! [2]: http://docs.rs/elliptic-curve
 
 #![no_std]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, intra_doc_link_resolution_failure)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
-    html_root_url = "https://docs.rs/ecdsa/0.2.1"
+    html_root_url = "https://docs.rs/ecdsa/0.3.0"
 )]
 
 // Re-export the `generic-array` crate
@@ -41,5 +52,5 @@ pub mod fixed_signature;
 #[cfg(feature = "test-vectors")]
 pub mod test_vectors;
 
-pub use self::{asn1_signature::Asn1Signature, curve::Curve, fixed_signature::FixedSignature};
-pub use elliptic_curve::weierstrass::{PublicKey, SecretKey};
+pub use self::{asn1_signature::Asn1Signature, fixed_signature::FixedSignature};
+pub use elliptic_curve::weierstrass::{Curve, PublicKey, SecretKey};
