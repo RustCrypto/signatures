9cba7dff2fc017bc6dcbeb60f76f1f2e5215f798
Tony Arcieri <bascule@gmail.com>
2020-07-31 10:24:04 -0700

    ecdsa: Verifier facade (gated under `verifier` feature) (#110)
    Adds a `ecdsa::verifier::Verifier` facade which handles parsing of
    `PublicKey` and computing digests of messages, ala the `Signer` facade
    added in #109.
    
    The implementation is generic over the elliptic curve and digest
    function, providing a `DigestVerifier` impl, and when the curve type
    impls `DigestPrimitive`, a `Verifier` impl as well.

---
+++ ecdsa/src/verifier.rs
@@ -0,0 +1,71 @@
+//! ECDSA verifier. Generic over elliptic curves.
+//!
+//! Requires an [`elliptic_curve::Arithmetic`] impl on the curve, and a
+//! [`VerifyPrimitive`] impl on its associated `AffinePoint` type.
+
+use crate::{
+    hazmat::{DigestPrimitive, VerifyPrimitive},
+    Error, Signature, SignatureSize,
+};
+use core::ops::Add;
+use elliptic_curve::{
+    consts::U1,
+    generic_array::ArrayLength,
+    weierstrass::{CompressedPointSize, Curve, FromPublicKey, PublicKey, UncompressedPointSize},
+    Arithmetic,
+};
+use signature::{digest::Digest, DigestVerifier};
+
+/// ECDSA verifier
+pub struct Verifier<C: Curve + Arithmetic> {
+    public_key: C::AffinePoint,
+}
+
+impl<C> Verifier<C>
+where
+    C: Curve + Arithmetic,
+    C::AffinePoint: VerifyPrimitive<C> + FromPublicKey<C>,
+    C::ElementSize: Add<U1>,
+    <C::ElementSize as Add>::Output: Add<U1>,
+    CompressedPointSize<C>: ArrayLength<u8>,
+    UncompressedPointSize<C>: ArrayLength<u8>,
+    SignatureSize<C>: ArrayLength<u8>,
+{
+    /// Create a new verifier
+    pub fn new(public_key: &PublicKey<C>) -> Result<Self, Error> {
+        let affine_point = C::AffinePoint::from_public_key(public_key);
+
+        if affine_point.is_some().into() {
+            Ok(Self {
+                public_key: affine_point.unwrap(),
+            })
+        } else {
+            Err(Error::new())
+        }
+    }
+}
+
+impl<C, D> DigestVerifier<D, Signature<C>> for Verifier<C>
+where
+    C: Curve + Arithmetic,
+    D: Digest<OutputSize = C::ElementSize>,
+    C::AffinePoint: VerifyPrimitive<C>,
+    SignatureSize<C>: ArrayLength<u8>,
+{
+    fn verify_digest(&self, digest: D, signature: &Signature<C>) -> Result<(), Error> {
+        self.public_key
+            .verify_prehashed(&digest.finalize(), signature)
+    }
+}
+
+impl<C> signature::Verifier<Signature<C>> for Verifier<C>
+where
+    C: Curve + Arithmetic + DigestPrimitive,
+    C::AffinePoint: VerifyPrimitive<C>,
+    C::Digest: Digest<OutputSize = C::ElementSize>,
+    SignatureSize<C>: ArrayLength<u8>,
+{
+    fn verify(&self, msg: &[u8], signature: &Signature<C>) -> Result<(), Error> {
+        self.verify_digest(C::Digest::new().chain(msg), signature)
+    }
+}
