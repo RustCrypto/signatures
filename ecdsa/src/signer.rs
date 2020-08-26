2e5ec4e25e61397f94d1f7ff9b2869ef17dcbe11
Tony Arcieri <bascule@gmail.com>
2020-08-26 15:27:06 -0700

    ecdsa: implement RFC6979 ephemeral scalar generation
    Adds a deterministic signing mode based on RFC6979.
    
    Modifies the existing `RandomizedDigestSigner` and `RandomizedSigner`
    impls to also use an RFC6979-style derivation, but supplying added
    entropy derived from a provided RNG, per an RFC6979 variant described in
    Section 3.6.

--- ecdsa/src/signer.rs
+++ ecdsa/src/signer.rs
@@ -3,28 +3,27 @@
 //! Requires an [`elliptic_curve::Arithmetic`] impl on the curve, and a
 //! [`SignPrimitive`] impl on its associated `Scalar` type.
 
-// TODO(tarcieri): RFC 6979; support for hardware crypto accelerators
+// TODO(tarcieri): support for hardware crypto accelerators
+
+mod rfc6979;
 
 use crate::{
     hazmat::{DigestPrimitive, SignPrimitive},
     Error, Signature, SignatureSize,
 };
 use elliptic_curve::{
-    generic_array::ArrayLength,
-    ops::Invert,
-    weierstrass::Curve,
-    zeroize::{Zeroize, Zeroizing},
-    Arithmetic, FromBytes, SecretKey,
+    generic_array::ArrayLength, ops::Invert, scalar::NonZeroScalar, weierstrass::Curve,
+    zeroize::Zeroize, Arithmetic, ElementBytes, FromBytes, FromDigest, SecretKey,
+};
+use signature::{
+    digest::{BlockInput, Digest, FixedOutput, Reset, Update},
+    DigestSigner,
 };
 
 #[cfg(feature = "rand")]
-use {
-    elliptic_curve::Generate,
-    signature::{
-        digest::Digest,
-        rand_core::{CryptoRng, RngCore},
-        RandomizedDigestSigner, RandomizedSigner,
-    },
+use signature::{
+    rand_core::{CryptoRng, RngCore},
+    RandomizedDigestSigner, RandomizedSigner,
 };
 
 /// ECDSA signer
@@ -34,7 +33,7 @@ where
     C::Scalar: Invert<Output = C::Scalar> + SignPrimitive<C> + Zeroize,
     SignatureSize<C>: ArrayLength<u8>,
 {
-    secret_scalar: C::Scalar,
+    secret_scalar: NonZeroScalar<C>,
 }
 
 impl<C> Signer<C>
@@ -45,8 +44,9 @@ where
 {
     /// Create a new signer
     pub fn new(secret_key: &SecretKey<C>) -> Result<Self, Error> {
-        let scalar = C::Scalar::from_bytes(secret_key.as_bytes());
+        let scalar = NonZeroScalar::from_bytes(secret_key.as_bytes());
 
+        // TODO(tarcieri): replace with into conversion when available (see subtle#73)
         if scalar.is_some().into() {
             Ok(Self {
                 secret_scalar: scalar.unwrap(),
@@ -57,24 +57,75 @@ where
     }
 }
 
+impl<C, D> DigestSigner<D, Signature<C>> for Signer<C>
+where
+    C: Curve + Arithmetic,
+    C::Scalar: FromDigest<C> + Invert<Output = C::Scalar> + SignPrimitive<C> + Zeroize,
+    D: BlockInput<BlockSize = C::ElementSize>
+        + FixedOutput<OutputSize = C::ElementSize>
+        + Clone
+        + Default
+        + Reset
+        + Update,
+    ElementBytes<C>: Zeroize,
+    SignatureSize<C>: ArrayLength<u8>,
+{
+    /// Sign message prehash using a deterministic ephemeral scalar (`k`)
+    /// computed using the algorithm described in RFC 6979 (Section 3.2):
+    /// <https://tools.ietf.org/html/rfc6979#section-3>
+    fn try_sign_digest(&self, digest: D) -> Result<Signature<C>, Error> {
+        let ephemeral_scalar = rfc6979::generate_k(&self.secret_scalar, digest.clone(), &[]);
+
+        self.secret_scalar
+            .as_ref()
+            .try_sign_prehashed(ephemeral_scalar.as_ref(), &digest.finalize())
+    }
+}
+
+impl<C> signature::Signer<Signature<C>> for Signer<C>
+where
+    C: Curve + Arithmetic + DigestPrimitive,
+    C::Scalar: Invert<Output = C::Scalar> + SignPrimitive<C> + Zeroize,
+    SignatureSize<C>: ArrayLength<u8>,
+    Self: DigestSigner<C::Digest, Signature<C>>,
+{
+    fn try_sign(&self, msg: &[u8]) -> Result<Signature<C>, signature::Error> {
+        self.try_sign_digest(C::Digest::new().chain(msg))
+    }
+}
+
 #[cfg(feature = "rand")]
 #[cfg_attr(docsrs, doc(cfg(feature = "rand")))]
 impl<C, D> RandomizedDigestSigner<D, Signature<C>> for Signer<C>
 where
     C: Curve + Arithmetic,
-    D: Digest<OutputSize = C::ElementSize>,
-    C::Scalar: Invert<Output = C::Scalar> + Generate + SignPrimitive<C> + Zeroize,
+    C::Scalar: FromDigest<C> + Invert<Output = C::Scalar> + SignPrimitive<C> + Zeroize,
+    D: BlockInput<BlockSize = C::ElementSize>
+        + FixedOutput<OutputSize = C::ElementSize>
+        + Clone
+        + Default
+        + Reset
+        + Update,
+    ElementBytes<C>: Zeroize,
     SignatureSize<C>: ArrayLength<u8>,
 {
+    /// Sign message prehash using an ephemeral scalar (`k`) derived according
+    /// to a variant of RFC 6979 (Section 3.6) which supplies additional
+    /// entropy from an RNG.
     fn try_sign_digest_with_rng(
         &self,
-        rng: impl CryptoRng + RngCore,
+        mut rng: impl CryptoRng + RngCore,
         digest: D,
     ) -> Result<Signature<C>, Error> {
-        let ephemeral_scalar = Zeroizing::new(C::Scalar::generate(rng));
+        let mut added_entropy = ElementBytes::<C>::default();
+        rng.fill_bytes(&mut added_entropy);
+
+        let ephemeral_scalar =
+            rfc6979::generate_k(&self.secret_scalar, digest.clone(), &added_entropy);
 
         self.secret_scalar
-            .try_sign_prehashed(&*ephemeral_scalar, &digest.finalize())
+            .as_ref()
+            .try_sign_prehashed(ephemeral_scalar.as_ref(), &digest.finalize())
     }
 }
 
@@ -83,9 +134,9 @@ where
 impl<C> RandomizedSigner<Signature<C>> for Signer<C>
 where
     C: Curve + Arithmetic + DigestPrimitive,
-    C::Digest: Digest<OutputSize = C::ElementSize>,
-    C::Scalar: Invert<Output = C::Scalar> + Generate + SignPrimitive<C> + Zeroize,
+    C::Scalar: Invert<Output = C::Scalar> + SignPrimitive<C> + Zeroize,
     SignatureSize<C>: ArrayLength<u8>,
+    Self: RandomizedDigestSigner<C::Digest, Signature<C>>,
 {
     fn try_sign_with_rng(
         &self,
