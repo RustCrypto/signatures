3e2aa3865f0ac3f0446a0934094f5640a59695c7
Tony Arcieri <bascule@gmail.com>
2020-12-22 08:07:18 -0800

    ecdsa v0.10.2

--- ecdsa/src/lib.rs
+++ ecdsa/src/lib.rs
@@ -48,7 +48,7 @@
 #![warn(missing_docs, rust_2018_idioms)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
-    html_root_url = "https://docs.rs/ecdsa/0.10.1"
+    html_root_url = "https://docs.rs/ecdsa/0.10.2"
 )]
 
 #[cfg(feature = "alloc")]
