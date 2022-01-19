c464cb9a0da9f043c0c1a6ba0fb20527f24f76c9
Tony Arcieri <bascule@gmail.com>
2022-01-19 15:20:31 -0700

    ecdsa v0.14.0-pre.1

--- ecdsa/src/lib.rs
+++ ecdsa/src/lib.rs
@@ -38,7 +38,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/ecdsa/0.14.0-pre.0"
+    html_root_url = "https://docs.rs/ecdsa/0.14.0-pre.1"
 )]
 
 #[cfg(feature = "alloc")]
