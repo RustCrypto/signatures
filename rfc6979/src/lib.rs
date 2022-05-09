fb29f79cb1a25376227b811e6f8439a5beb6bc38
Tony Arcieri <bascule@gmail.com>
2022-05-08 22:36:34 -0600

    rfc6979 v0.2.0

--- rfc6979/src/lib.rs
+++ rfc6979/src/lib.rs
@@ -10,8 +10,7 @@
 #![warn(missing_docs, rust_2018_idioms)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/rfc6979/0.1.0"
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
 )]
 
 use crypto_bigint::{ArrayEncoding, ByteArray, Integer};
