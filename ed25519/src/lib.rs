2c75d59928e5672b044254663b2d9e54ffb28dcb
Tony Arcieri <bascule@gmail.com>
2022-05-16 08:55:59 -0600

    ed25519: factor `serde` support into its own module (#481)

--- ed25519/src/lib.rs
+++ ed25519/src/lib.rs
@@ -262,6 +262,9 @@ extern crate alloc;
 #[cfg_attr(docsrs, doc(cfg(feature = "pkcs8")))]
 pub mod pkcs8;
 
+#[cfg(feature = "serde")]
+mod serde;
+
 pub use signature::{self, Error};
 
 #[cfg(feature = "pkcs8")]
@@ -271,10 +274,6 @@ use core::{fmt, str};
 
 #[cfg(feature = "alloc")]
 use alloc::vec::Vec;
-#[cfg(feature = "serde")]
-use serde::{de, ser, Deserialize, Serialize};
-#[cfg(feature = "serde_bytes")]
-use serde_bytes_crate as serde_bytes;
 
 /// Length of an Ed25519 signature in bytes.
 #[deprecated(since = "1.3.0", note = "use ed25519::Signature::BYTE_SIZE instead")]
@@ -412,6 +411,7 @@ impl fmt::UpperHex for Signature {
 ///
 /// Upper and lower case hexadecimal are both accepted, however mixed case is
 /// rejected.
+// TODO(tarcieri): use `base16ct`?
 impl str::FromStr for Signature {
     type Err = Error;
 
@@ -449,100 +449,5 @@ impl str::FromStr for Signature {
         }
 
         Self::try_from(&result[..])
-    }
-}
-
-#[cfg(feature = "serde")]
-impl Serialize for Signature {
-    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        use ser::SerializeTuple;
-
-        let mut seq = serializer.serialize_tuple(Signature::BYTE_SIZE)?;
-
-        for byte in &self.0[..] {
-            seq.serialize_element(byte)?;
-        }
-
-        seq.end()
-    }
-}
-
-// serde lacks support for deserializing arrays larger than 32-bytes
-// see: <https://github.com/serde-rs/serde/issues/631>
-#[cfg(feature = "serde")]
-impl<'de> Deserialize<'de> for Signature {
-    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        struct ByteArrayVisitor;
-
-        impl<'de> de::Visitor<'de> for ByteArrayVisitor {
-            type Value = [u8; Signature::BYTE_SIZE];
-
-            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-                formatter.write_str("bytestring of length 64")
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<[u8; Signature::BYTE_SIZE], A::Error>
-            where
-                A: de::SeqAccess<'de>,
-            {
-                use de::Error;
-                let mut arr = [0u8; Signature::BYTE_SIZE];
-
-                for (i, byte) in arr.iter_mut().enumerate() {
-                    *byte = seq
-                        .next_element()?
-                        .ok_or_else(|| Error::invalid_length(i, &self))?;
-                }
-
-                Ok(arr)
-            }
-        }
-
-        deserializer
-            .deserialize_tuple(Signature::BYTE_SIZE, ByteArrayVisitor)
-            .and_then(|bytes| bytes.try_into().map_err(de::Error::custom))
-    }
-}
-
-#[cfg(feature = "serde_bytes")]
-impl serde_bytes::Serialize for Signature {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(&self.0)
-    }
-}
-
-#[cfg(feature = "serde_bytes")]
-impl<'de> serde_bytes::Deserialize<'de> for Signature {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        struct ByteArrayVisitor;
-
-        impl<'de> de::Visitor<'de> for ByteArrayVisitor {
-            type Value = [u8; Signature::BYTE_SIZE];
-
-            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-                formatter.write_str("bytestring of length 64")
-            }
-
-            fn visit_bytes<E>(self, bytes: &[u8]) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                use de::Error;
-
-                bytes
-                    .try_into()
-                    .map_err(|_| Error::invalid_length(bytes.len(), &self))
-            }
-        }
-
-        deserializer
-            .deserialize_bytes(ByteArrayVisitor)
-            .and_then(|bytes| bytes.try_into().map_err(de::Error::custom))
     }
 }
