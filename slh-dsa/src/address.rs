6193fbfcf63e4cad805cc18ffe6b6c321667cbf0
Tony Arcieri <bascule@gmail.com>
2024-08-16 09:51:08 -0600

    slh-dsa: enable `derive` feature of `zerocopy`
    ...rather than directly depending on `zerocopy-derive`.
    
    This fixes problems that arise if the `derive` feature is enabled by
    other dependencies in the same tree. See #846.

--- slh-dsa/src/address.rs
+++ slh-dsa/src/address.rs
@@ -20,7 +20,6 @@ use typenum::U22;
 
 use zerocopy::byteorder::big_endian::{U32, U64};
 use zerocopy::AsBytes;
-use zerocopy_derive::AsBytes;
 
 /// `Address` represents a hash address as defined by FIPS-205 section 4.2
 pub trait Address: AsRef<[u8]> {
