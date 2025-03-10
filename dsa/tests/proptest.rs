09e7676a7b73e334cd97655768c6d450f61e19cf
Arthur Gautier <arthur.gautier@arista.com>
2025-03-09 20:03:31 -0700

    dsa: check signature verification with proptest

---
+++ dsa/tests/proptest.rs
@@ -0,0 +1,47 @@
+#![cfg(feature = "hazmat")]
+//! Property-based tests.
+
+use der::{Decode, Encode, Sequence, asn1::Uint};
+use dsa::{Components, KeySize, Signature, SigningKey, VerifyingKey, signature::Verifier};
+use pkcs8::DecodePublicKey;
+use proptest::prelude::*;
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+#[derive(Sequence)]
+struct MockSignature {
+    r: Uint,
+    s: Uint,
+}
+
+const OPENSSL_PEM_PUBLIC_KEY: &str = include_str!("pems/public.pem");
+
+prop_compose! {
+    fn private_key()(seed in any::<[u8; 32]>()) -> SigningKey {
+        let mut rng = ChaCha8Rng::from_seed(seed);
+        #[allow(deprecated)]
+        let components = Components::generate(&mut rng, KeySize::DSA_1024_160);
+        SigningKey::generate(&mut rng, components)
+    }
+}
+
+proptest! {
+    #[test]
+    fn dsa_signature_verification( r in any::<Vec<u8>>(), s in any::<Vec<u8>>(),) {
+        let verifying_key = VerifyingKey::from_public_key_pem(OPENSSL_PEM_PUBLIC_KEY)
+            .expect("Failed to decode PEM encoded OpenSSL public key");
+
+        let asn1 = MockSignature {
+            r: Uint::new(&r).unwrap(),
+            s: Uint::new(&s).unwrap(),
+        }
+        .to_der()
+        .expect("Failed to serialize signature");
+
+        let Ok(signature) = Signature::from_der(&asn1) else {
+            return Ok(());
+        };
+
+        prop_assert!(verifying_key.verify(&[], &signature).is_err());
+    }
+}
