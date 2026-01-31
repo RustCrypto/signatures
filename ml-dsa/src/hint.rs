302411851f8f95b85519d6ad064053be1d9dee25
Tony Arcieri <bascule@gmail.com>
2026-01-31 12:00:16 -0700

    cleanups and preserve original logic

--- ml-dsa/src/hint.rs
+++ ml-dsa/src/hint.rs
@@ -21,10 +21,17 @@ fn use_hint<TwoGamma2: Unsigned>(h: bool, r: Elem) -> Elem {
     let m: u32 = (BaseField::Q - 1) / TwoGamma2::U32;
     let (r1, r0) = r.decompose::<TwoGamma2>();
     let gamma2 = TwoGamma2::U32 / 2;
-    if h && r0.0 > 0 && r0.0 <= gamma2 {
-        Elem::new((r1.0 + 1) % m)
-    } else if h {
-        Elem::new((r1.0 + m - 1) % m)
+
+    if h {
+        if r0.0 > 0 && r0.0 <= gamma2 {
+            Elem::new((r1.0 + 1) % m)
+        } else if (r0.0 == 0) || (r0.0 >= BaseField::Q - gamma2) {
+            Elem::new((r1.0 + m - 1) % m)
+        } else {
+            // We use the Elem encoding even for signed integers.  Since r0 is computed
+            // mod+- 2*gamma2 (possibly minus 1), it is guaranteed to be in [-gamma2, gamma2].
+            unreachable!();
+        }
     } else {
         r1
     }
