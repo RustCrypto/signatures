9b5a40c8137b88d144068f4da2863cb9bb818a30
Tony Arcieri <bascule@gmail.com>
2026-05-06 10:00:44 -0600

    ed448: use `serdect` for `serde` support
    Like #1324 did for `ed25519`, this uses the `serdect` crate for the
    `serde` implementation, which is a breaking change.
    
    See comments in #1324 for more information.

--- ed448/src/serde.rs
+++ ed448/src/serde.rs
@@ -1,54 +1,28 @@
-//! `serde` support.
+//! `serde` support including optional `serde_bytes` support.
 
 use crate::{Signature, SignatureBytes};
-use ::serde::{Deserialize, Serialize, de, ser};
 use core::fmt;
+use serdect::serde::{Deserialize, Serialize, de, ser};
 
+#[cfg(feature = "serde")]
 impl Serialize for Signature {
-    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        use ser::SerializeTuple;
-
-        let mut seq = serializer.serialize_tuple(Signature::BYTE_SIZE)?;
-
-        for byte in self.to_bytes() {
-            seq.serialize_element(&byte)?;
-        }
-
-        seq.end()
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serdect::array::serialize_hex_upper_or_bin(&self.to_bytes(), serializer)
     }
 }
 
+#[cfg(feature = "serde")]
 impl<'de> Deserialize<'de> for Signature {
-    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        struct ByteArrayVisitor;
-
-        impl<'de> de::Visitor<'de> for ByteArrayVisitor {
-            type Value = [u8; Signature::BYTE_SIZE];
-
-            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-                formatter.write_str("bytestring of length 64")
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<[u8; Signature::BYTE_SIZE], A::Error>
-            where
-                A: de::SeqAccess<'de>,
-            {
-                use de::Error;
-                let mut arr = [0u8; Signature::BYTE_SIZE];
-
-                for (i, byte) in arr.iter_mut().enumerate() {
-                    *byte = seq
-                        .next_element()?
-                        .ok_or_else(|| Error::invalid_length(i, &self))?;
-                }
-
-                Ok(arr)
-            }
-        }
-
-        deserializer
-            .deserialize_tuple(Signature::BYTE_SIZE, ByteArrayVisitor)
-            .map(Into::into)
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let mut bytes = [0u8; Signature::BYTE_SIZE];
+        serdect::array::deserialize_hex_or_bin(&mut bytes, deserializer)?;
+        Ok(bytes.into())
     }
 }
 
@@ -56,7 +30,7 @@ impl<'de> Deserialize<'de> for Signature {
 impl serde_bytes::Serialize for Signature {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: serde::Serializer,
+        S: ser::Serializer,
     {
         serializer.serialize_bytes(&self.to_bytes())
     }
